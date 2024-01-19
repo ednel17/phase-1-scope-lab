f3a6717f53d54e0d38d8abd8e61f25b45814cf4b
Carl <carljeanpierre@gmail.com>
2024-01-19 03:15:10 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+
+function getCustomerName(){
+    return window.customerName;
+}
+
+function upperCaseCustomerName(){
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'jack';
+    leastFavoriteCustomer = 'dawson';
+    
+}
